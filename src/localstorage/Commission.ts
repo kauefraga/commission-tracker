98ed98453541cb9ced16963a85775679206ffa69
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2024-01-08 20:09:08 -0300

    :sparkles: feat: create an api to manage localstorage and commissions

---
+++ src/localstorage/Commission.ts
@@ -0,0 +1,41 @@
+import type { Commission } from '@/types/Commission';
+import { v4, validate } from 'uuid';
+
+export function createCommission(commission: Commission) {
+  if (!commission.id) {
+    commission.id = v4();
+  }
+
+  if (!commission.created_at) {
+    commission.created_at = new Date();
+  }
+
+  localStorage.setItem(commission.id, JSON.stringify(commission));
+}
+
+export function getAllCommissions() {
+  const storedCommissions: Commission[] = [];
+  let storedIncome = 0;
+
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+
+    if (!key) throw new Error('Commission out of range. :(');
+
+    if (validate(key)) {
+      const json = localStorage.getItem(key);
+
+      if (!json) throw new Error('Commission does not exist.');
+
+      const commission = JSON.parse(json) as Commission;
+
+      storedCommissions.push(commission);
+      storedIncome += commission.price
+    }
+  }
+
+  return {
+    storedCommissions,
+    storedIncome
+  }
+}
