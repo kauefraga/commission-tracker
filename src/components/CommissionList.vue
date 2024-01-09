138a16114dcbfa13eee0c36f13302059bf938735
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2024-01-09 12:41:03 -0300

    :sparkles: feat: create a list component

---
+++ src/components/CommissionList.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { Commission } from '@/types/Commission';
+
+defineProps<{
+  commissions: Commission[]
+}>();
+</script>
+
+<template>
+  <ul
+    id="commission-list"
+    class="my-6"
+  >
+    <li
+      class="
+        flex justify-between bg-neutral-200 hover:bg-neutral-300 active:bg-neutral-400
+        text-lg rounded-lg px-12 py-4 m-2
+      "
+      v-for="commission in commissions"
+      v-bind:key="commission.client"
+    >
+      <p class="p-1">{{ commission.client }}</p>
+      <p class="p-1">Price (R$): {{ commission.price }}</p>
+    </li>
+  </ul>
+</template>
