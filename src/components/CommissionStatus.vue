5b9cc003f004dbeaea1240ba6055213c35ed034f
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2024-01-12 17:45:19 -0300

    :sparkles: feat: create a component to show commission status as colored balls

---
+++ src/components/CommissionStatus.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { Status } from '@/types/Commission';
+import { toRefs } from 'vue';
+
+const props = defineProps<{
+  commissionStatus: Status
+}>();
+const { commissionStatus } = toRefs(props);
+
+const statusColor = {
+  'NONE': 'bg-violet-900',
+  'STARTED': 'bg-pink-500',
+  'ALMOST DONE': 'bg-yellow-600',
+  'DONE': 'bg-lime-600'
+};
+
+</script>
+
+<template>
+  <p class="flex items-center">
+    <span
+      class="min-w-6 min-h-6 rounded-full inline-block"
+      :class=statusColor[commissionStatus]
+    ></span>
+  </p>
+</template>
