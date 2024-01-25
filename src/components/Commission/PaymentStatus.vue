492983ef82a49669dabc79cc35841a747ecb4316
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2024-01-25 00:32:53 -0300

    :sparkles: feat: create PaymentStatus component

---
+++ src/components/Commission/PaymentStatus.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { PaymentStatus } from '@/types/Commission';
+import { toRefs } from 'vue';
+
+const props = defineProps<{
+  paymentStatus: PaymentStatus
+}>();
+const { paymentStatus } = toRefs(props);
+
+const paymentSign = {
+  'NOT PAID': '$',
+  'HALF PAID': '$$',
+  'PAID': '$$$',
+};
+
+</script>
+
+<template>
+  <p
+    class="font-bold text-xl text-lime-600 saturate-50"
+  >
+    {{ paymentSign[paymentStatus] }}
+  </p>
+</template>
